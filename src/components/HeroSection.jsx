f7dd991441caf341690c6f1d699a5695f8b8f90e
himanshu <hs386778@gmail.com>
2024-11-09 12:47:15 +0530

    making a resume page

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -2,11 +2,12 @@ function HeroSection() {
   return (
     <>
       <div className="w-full flex justify-center">
+        <div className="w-5/6 flex">
         <div className="w-1/2 flex justify-center items-center">
         <div className="heroContent flex flex-col justify-between w-full h-56">
-            <h1 className="text-6xl font-semibold uppercase">Hi! I'm Himanshu <span className="text-red-600">Sharma</span></h1>
-            <p className="heroPara text-xl">
-            I'm a passionate frontend developer dedicated to crafting responsive, user-friendly websites. With a foundation in modern web technologies, I bring ideas to life with clean code and innovative design.
+            <h1 className="text-5xl font-semibold uppercase heroName">Hi! I&apos;m Himanshu <span className="text-red-600">Sharma</span></h1>
+            <p className="heroPara text-2xl select-none">
+            I&apos;m a passionate frontend developer dedicated to crafting responsive, user-friendly websites. With a foundation in modern web technologies, I bring ideas to life with clean code and innovative design.
             </p>
         </div>
         </div>
@@ -18,6 +19,7 @@ function HeroSection() {
             width="100%"
             height="100%"
           />
+        </div>
         </div>
       </div>
     </>
